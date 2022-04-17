3f2fcc1a3e1142e41fbd101589ce89f24fbb3641
Paul513607 <paul.samson42@gmail.com>
2022-04-17 23:00:52 +0300

    fixed href and formatting

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -4,7 +4,7 @@ const startEvents = () => {
     document.getElementById("existing-acc").onclick = () => {
         location.href = "/TW_ManagingWater/login/login.html";
     }
-    
+
     document.getElementById("new-acc").onclick = () => {
         location.href = "/TW_ManagingWater/signIn/signIn.html";
     }
@@ -13,50 +13,52 @@ const startEvents = () => {
 }
 
 const getRndInteger = (min, max) => {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const randomClouds = () => {
-    picture=document.getElementById("image1");
+    picture = document.getElementById("image1");
 
-    spaceW=-400 - picture.height;
-    spaceH=500 - picture.width;
+    spaceW = -400 - picture.height;
+    spaceH = 500 - picture.width;
 
     moveIt();
-    Init2(); Init3(); Init4();
+    Init2();
+    Init3();
+    Init4();
 }
 
 const Init2 = () => {
-    picture=document.getElementById("image2");
+    picture = document.getElementById("image2");
 
-    spaceW=-400 - picture.height;
-    spaceH=500 - picture.width;
-    if(spaceH<0) spaceH=10;
-   
+    spaceW = -400 - picture.height;
+    spaceH = 500 - picture.width;
+    if (spaceH < 0) spaceH = 10;
+
     moveIt();
 }
 
 const Init3 = () => {
-    picture=document.getElementById("image3");
+    picture = document.getElementById("image3");
 
-    spaceW=600 - picture.height;
-    spaceH=500 - picture.width;
-    if(spaceH<0) spaceH=10;
-    if(spaceW<0) spaceW=10;
+    spaceW = 600 - picture.height;
+    spaceH = 500 - picture.width;
+    if (spaceH < 0) spaceH = 10;
+    if (spaceW < 0) spaceW = 10;
 
     moveIt();
 }
 
 const Init4 = () => {
-    picture=document.getElementById("image4");
+    picture = document.getElementById("image4");
 
-    spaceW=600 - picture.height;
-    spaceH=500 - picture.width;
+    spaceW = 600 - picture.height;
+    spaceH = 500 - picture.width;
 
     moveIt();
 }
 
 const moveIt = () => {
-    picture.style.top=Math.round(Math.random() * spaceH) + 'px';
-    picture.style.left=Math.round(Math.random() *spaceW) + 'px';
+    picture.style.top = Math.round(Math.random() * spaceH) + 'px';
+    picture.style.left = Math.round(Math.random() * spaceW) + 'px';
 }
--- signIn/signIn.js
+++ signIn/signIn.js
@@ -1,60 +1,60 @@
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
-
-function randomClouds()
-{
-    picture=document.getElementById("image1");
-
-    spaceW=-400 - picture.height;
-    spaceH=500 - picture.width;
-    
-    
-
-    moveIt();
-    Init2();Init3();Init4();
-
+    return Math.floor(Math.random() * (max - min)) + min;
 }
-function Init2()
-{
-    picture=document.getElementById("image2");
 
-    spaceW=-400 - picture.height;
-    spaceH=500 - picture.width;
-    if(spaceH<0) spaceH=10;
-   
+function randomClouds() {
+    picture = document.getElementById("image1");
 
-    moveIt();
-   
+    spaceW = -400 - picture.height;
+    spaceH = 500 - picture.width;
 
-}
-function Init3()
-{
-    picture=document.getElementById("image3");
 
-    spaceW=600 - picture.height;
-    spaceH=500 - picture.width;
-    if(spaceH<0) spaceH=10;
-    if(spaceW<0) spaceW=10;
 
     moveIt();
-    
-
-}
-function Init4()
-{
-    picture=document.getElementById("image4");
-
-    spaceW=600 - picture.height;
-    spaceH=500 - picture.width;
-    
-
-    moveIt();
+    Init2();
+    Init3();
+    Init4();
 
 }
 
-function moveIt()
-{
-    picture.style.top=Math.round(Math.random() * spaceH) + 'px';
-    picture.style.left=Math.round(Math.random() *spaceW) + 'px';
+function Init2() {
+    picture = document.getElementById("image2");
+
+    spaceW = -400 - picture.height;
+    spaceH = 500 - picture.width;
+    if (spaceH < 0) spaceH = 10;
+
+
+    moveIt();
+
+
+}
+
+function Init3() {
+    picture = document.getElementById("image3");
+
+    spaceW = 600 - picture.height;
+    spaceH = 500 - picture.width;
+    if (spaceH < 0) spaceH = 10;
+    if (spaceW < 0) spaceW = 10;
+
+    moveIt();
+
+
+}
+
+function Init4() {
+    picture = document.getElementById("image4");
+
+    spaceW = 600 - picture.height;
+    spaceH = 500 - picture.width;
+
+
+    moveIt();
+
+}
+
+function moveIt() {
+    picture.style.top = Math.round(Math.random() * spaceH) + 'px';
+    picture.style.left = Math.round(Math.random() * spaceW) + 'px';
 }
